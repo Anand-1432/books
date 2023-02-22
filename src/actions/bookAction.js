d617bea8b40fdfe4765bff301ce62aa7d7da4819
Ananad Rao Gaikwad <123agaikwad@gmail.com>
2023-02-22 23:45:12 +0530

    updating

--- src/actions/bookAction.js
+++ src/actions/bookAction.js
@@ -1,12 +1,12 @@
 import { REQUEST_BOOKS, SUCCESS_BOOKS, ADD_BOOKS, MAKE_SUB } from "../constants/bookConstants";
 import axios from 'axios';
 
-export const addBook = (data) => async (dispatch) => {
+export const addBook = (bookData) => async (dispatch) => {
     try {
-        // console.log(formdata);
-        // const config = { headers: { "Content-Type": "multipart/form-data" } };
-        // const { data } = await axios.post('/add_book', formdata, config);
-        // console.log(data);
+        console.log(bookData);
+        const config = { headers: { "Content-Type": "application/json" } };
+        const { data } = await axios.post('/add_book', bookData, config);
+        console.log(data);
         dispatch({ type: ADD_BOOKS, payload: data });
     } catch (error) {
         console.log(error);
